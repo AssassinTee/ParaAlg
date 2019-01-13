3494496cd023fb526cf8477aef3ad8c436542a3d
Marvin Winkens <winkens2@jrl01.jureca>
2019-01-13 18:02:04 +0100

    working

--- uebung7_rueckwearts/rueckei_para.c
+++ uebung7_rueckwearts/rueckei_para.c
@@ -6,46 +6,61 @@
 /***************************************************/
 #include <stdlib.h>
 #include <stdio.h>
-#define mat_size 20
+#include <mpi.h>
+#define mat_size 17
 int main(int argc, char *argv[])
 {
   int i, j;
     int world_rank, world_size;
 
+  //double matrix[mat_size][mat_size];
+  //double vectorB[mat_size];
+  //double vectorX[mat_size];
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  double matrix[mat_size][mat_size];
-  double vectorB[mat_size];
+  if(mat_size%world_size != 0) {
+    MPI_Abort(MPI_COMM_WORLD, 42);
+    return 1;
+  }
+
+  int np = mat_size/world_size;
+  double matrix[np][mat_size];
   double vectorX[mat_size];
+  double vectorB[np];
 
-    for(int i = 0; i < mat_size; ++i)
+    for(int i = 0; i < np; ++i)
     {
+	int i2 = i*world_size+world_rank;
         for(int j = 0; j < mat_size; ++j)
         {
-            if(j < i)
+            if(j < i2)
                 matrix[i][j] = 0;
             else
                 matrix[i][j] = mat_size-j;
         }
-        b[i] = i+1;
+        vectorB[i] = i2+1;
     }
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    //MPI_Init(&argc, &argv);
+    //MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    //MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
 
   for(i=mat_size-1; i>=0; --i)
     {
         int root = i%world_size;
+	int i2 = i/world_size;
         if(root == world_rank) {
             /* Berechnung Element des Ergebnisvektors*/
-            vectorX[i] = vectorB[i] / matrix[i][i];
+            vectorX[i] = vectorB[i2] / matrix[i2][i];
             //TODO send
         }
         MPI_Bcast(&vectorX[i], 1, MPI_DOUBLE, root, MPI_COMM_WORLD);
           /* Update mit gerade berechnetem Ergebnis */
           /* Update every worldrank+k*worldsize spalte*/
-          for(j=world_rank; j<mat_size; j+=world_size)
+          for(j=0; j < mat_size/world_size; ++j)
             vectorB[j] -= matrix[j][i]*vectorX[i];
     }
 
@@ -53,7 +68,7 @@ int main(int argc, char *argv[])
   /* Ausgabe */
     if(world_rank == 0) {
         printf("Das Ergebnis lautet:\n");
-        for(i=0;i<4;++i)
+        for(i=0;i<mat_size;++i)
             printf("  %f  ",vectorX[i]);
         printf("\n");
     }
