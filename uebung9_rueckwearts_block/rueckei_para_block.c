1639db190cc1e015c255270f83957549c47b3c56
Essigautomat <mw1996oede@googlemail.com>
2019-01-13 18:54:56 +0100

    fixed?

--- uebung9_rueckwearts_block/rueckei_para_block.c
+++ uebung9_rueckwearts_block/rueckei_para_block.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
-#define mat_size 16
+#define glob_mat_size 16
 #define nb 2
 int main(int argc, char *argv[])
 {
@@ -21,63 +21,71 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if(mat_size%world_size != 0)
+    if(glob_mat_size%world_size != 0)
     {
         MPI_Abort(MPI_COMM_WORLD, 42);
         return 1;
     }
 
-    int blocks = mat_size/nb;
-    int local_blocks = blocks/world_size;
-    double matrix[local_blocks*nb][mat_size];
-    double vectorX[mat_size];
-    double vectorB[local_blocks*nb];
+    int glob_blocks = glob_mat_size/nb;
+    int local_blocks = glob_blocks/world_size;
+    int local_row_num = local_blocks*nb;
+    double matrix[local_row_num][mat_size];
+    double vectorX[glob_mat_size];
+    double vectorB[local_row_num];
 
     for(int i = 0; i < local_blocks; ++i)//iterate over blocks
     {
         for(int k = 0; k < nb; ++k)  //iteriere über zeilen im block
         {
-            int i2 = i*blocks+k;//globaler index i2, i-ter block, k-te zeile
-            int i3 = i*nb+k;//localer index i3, i-ter block, k-te spalte
-            for(int j = 0; j < mat_size; ++j)//iteriere über spalten
+            int glob_index_row = i*blocks+k;//globaler index i2, i-ter block, k-te zeile
+            int loc_index_row = i*nb+k;//localer index i3, i-ter block, k-te spalte
+            for(int col = 0; col < glob_mat_size; ++col)//iteriere über spalten
             {
-                if(j < i2)
-                    matrix[i3][j] = 0;
+                if(j < glob_index_row)
+                    matrix[loc_index_row][col] = 0;
                 else
-                    matrix[i3][j] = mat_size-j;
+                    matrix[loc_index_row][col] = glob_mat_size-j;
             }
-            vectorB[i3] = i2+1;
+            vectorB[loc_index_row] = i2+1;
         }
     }
-
-    //MPI_Init(&argc, &argv);
-    //MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    //MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
 
     for(i=blocks-1; i>=0; --i)
     {
         int root = i%world_size;//root ist block%world_size
-        int i2 = i/world_size;//i2 ist der i-te block vom root prozess
+        int loc_block_index = i/world_size;//i2 ist der i-te block vom root prozess
         if(root == world_rank)
         {
             /* Berechnung Block des Ergebnisvektors*/
-            for(int k=nb-1; k>= 0; --k)
+            for(int off=nb-1; off>= 0; --off)//offset ist zeilenindex des blocks
             {
-                vectorX[i+k] = vectorB[i2] / matrix[i2][i+k];
-                //Update
-                for(j = k-1; j >= 0; --j)
-                    vectorB[j] -= matrix[j][i+k]*vectorX[i+k];
+                //Berechne indizies
+                int glob_index_row = nb*i+off;
+                int loc_index_row = loc_block_index+off;
+
+                //Berechne Matrix vector
+                vectorX[glob_index_row] = vectorB[loc_index_row] / matrix[loc_index_row][glob_index_row];
+
+                //Update B Vektor NUR am lokalen Block
+                for(uoff = off-1; uoff >= 0; --uoff) {//uoff = update offset
+                    int loc_index_row_update = loc_index_row+uoff;
+                    vectorB[loc_index_row_update] -= matrix[loc_index_row_update][glob_index_row]*vectorX[glob_index_row];
+                }
             }
-            //TODO send
         }
+        /*Broadcast Block i (size=nb) an alle*/
         MPI_Bcast(&vectorX[i], nb, MPI_DOUBLE, root, MPI_COMM_WORLD);
-        /* Update mit gerade berechnetem Ergebnis */
-        for(j=0; j < mat_size/world_size; ++j)//matrix vector mult
-        {
-            for(int k = nb-1; k >= 0; --k)
+        /* Update mit Block i */
+        if(root != world_rank) {//Root hat schon geupdatet!!!
+            for(j=0; j < mat_size/world_size; ++j)//matrix vector mult
             {
-                vectorB[j+k] -= matrix[j][i]*vectorX[i];
+                for(int off = nb-1; off >= 0; --off)//offset in block i
+                {
+                    int glob_index_row = nb*i+off;
+                    int loc_index_row = j;
+                    vectorB[loc_index_row] -= matrix[loc_index_row][glob_index_row]*vectorX[glob_index_row];
+                }
             }
         }
     }
